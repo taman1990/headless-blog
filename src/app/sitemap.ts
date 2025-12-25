3d693ed616457ec1a7d557ba44a11f26f2a1229e
taman sherzad <tamans.sherzad@gmail.com>
2025-12-26 00:57:06 +0100

    Include tag pages in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,21 @@
-import { getAllPosts } from "@/lib/posts";
+import { getAllPosts, getAllTags } from "@/lib/posts";
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  const siteUrl =
+    process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+
   const posts = getAllPosts();
+  const tags = getAllTags();
 
   const postEntries = posts.map((post) => ({
     url: `${siteUrl}/blog/${post.slug}`,
     lastModified: new Date(post.date),
+  }));
+
+  const tagEntries = tags.map((tag) => ({
+    url: `${siteUrl}/tags/${tag}`,
+    lastModified: new Date(),
   }));
 
   return [
@@ -19,6 +27,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${siteUrl}/blog`,
       lastModified: new Date(),
     },
+    {
+      url: `${siteUrl}/tags`,
+      lastModified: new Date(),
+    },
     ...postEntries,
+    ...tagEntries,
   ];
 }
